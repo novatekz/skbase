a6c9ac27106a3fec148f68c4d0df4aa850a756d1
aternyx <aterny@shuriken.pm>
2024-06-21 17:26:41 +0300

    rename load.php to init.php

---
+++ sk/init.php
@@ -0,0 +1,22 @@
+<?php
+
+// Disallow direct access to this file for security reasons
+if(!defined("SK"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure you use this within your skBase project. (or define SK if you know what you are doing.)");
+}
+
+// Fallback
+if(!defined('THIS_SCRIPT'))
+{
+	define('THIS_SCRIPT', 'unknown');
+}
+
+// Attempt root path autodetection
+if(!defined('SK_ROOT'))
+{
+	define('SK_ROOT', dirname(dirname(__FILE__))."/");
+}
+
+define("TIME_NOW", time()); // Set time variable
+
